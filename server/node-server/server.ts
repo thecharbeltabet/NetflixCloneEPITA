4c94bb1c89cbe7ed1402688ebc373ce8e91e71fd
Charbel Tabet <charbeltabet33@hotmail.com>
2023-02-13 19:55:48 +0200

    Added the route

---
+++ server/node-server/server.ts
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+const mongoURL = process.env.DB_URL;
+console.log(mongoURL)
+
+
+const routes = require('./Routes/ratingRoute');
+
+mongoose.connect(mongoURL, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+});
+
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+console.log(error);
+});
+
+database.once('connected', () => {
+
+console.log('Database Connected');
+});
+
+const app = express();
+
+app.use(cors({
+credentials: true,
+
+origin: 'http://localhost:3000',
+
+}));
+
+app.use(express.json());
+
+app.use('/api', routes);
+
+app.listen(process.env.PORT, () => {
+    
+console.log(`Server Started at ${4000}`);
+});
+
+
+
